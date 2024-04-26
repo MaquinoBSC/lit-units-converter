13e287656ddd0bc9d50dd4fb50f2736554459846
MaquinoBSC <martinare.ipn@gmail.com>
2024-04-26 16:03:42 -0600

    chore: creation of the component

---
+++ lit-units-converter.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit";
+
+class LitUnitsConverter extends LitElement {
+    render() {
+        return html`
+            <p>Martin</p>
+        `;
+    }
+}
+
+customElements.define('lit-units-converter', LitUnitsConverter);
